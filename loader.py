be0e3897d37d73bc240290dc4805762ed652c0e2
Samuel Showalter <samuelrshowalter@gmail.com>
2021-05-07 13:30:29 -0700

    Added next_epoch functionality. Still need a better tokenizer, Preethi is on that

--- loader.py
+++ loader.py
@@ -11,6 +11,8 @@
 #   Module Imports
 #################################################################################
 
+import os
+import sys
 import pandas as pd
 import numpy as np
 from glob import glob
@@ -83,6 +85,44 @@ class Loader(object):
 
         return data_dict
 
+    def __synthesize_data(self,batch_size):
+        """ Synthesize multilabel text dataset
+        and labels
+
+        :returns: Synthesized train and test dataset
+
+        """
+
+        # Get varying sample sizes
+        sample_sizes = np.random.randint(self.co_occ_range[0],
+                                         self.co_occ_range[1],
+                                         size = batch_size,
+                                         dtype = int)
+
+        #Sample only from specific crises
+        sample_crises = np.random.choice(self.train_crises,size = batch_size)
+
+        #Get specific random samples from every crisis
+        crisis_inds = [
+            np.random.randint(0, self.train_dict[c].shape[0],size = s)
+            for c,s in zip(sample_crises, sample_sizes)]
+
+        # Get a set of tuples joining sentences and labels together.
+        # These are unexpanded and are sent compressed to next_batch
+        # for expansion
+        crisis_tokens_labels = list(zip(*
+            [([self.tokenizer(sentence) for sentence
+               in self.train_dict[c].loc[s,"tweet_text"]],
+             self.train_dict[c].loc[s,"class_label"]) for c,s in
+            zip(sample_crises, crisis_inds)
+            ]))
+
+        # Separate crisis tokens and labels
+        crisis_tokens = crisis_tokens_labels[0]
+        crisis_labels = crisis_tokens_labels[1]
+
+        return crisis_tokens, crisis_labels
+
     def load_files(self, random_seed = 42):
         """Load files from directory
 
@@ -115,8 +155,8 @@ class Loader(object):
 
         #Make dictionary of crises
         self.train_dict = self.__make_crisis_dict(self.train_corpus, self.train_crises)
-        self.dev_dict = self.__make_crisis_dict(self.dev_dict, self.dev_crises)
-        self.test_dict = self.__make_crisis_dict(self.test_dict, self.test_crises)
+        self.dev_dict = self.__make_crisis_dict(self.dev_corpus, self.dev_crises)
+        self.test_dict = self.__make_crisis_dict(self.test_corpus, self.test_crises)
 
     def tokenize(self, sentence):
         """Tokenize input based on provided tokenizer
@@ -134,6 +174,12 @@ class Loader(object):
         :batch_size: size of training batch
         :returns: batch of data-augmented multi-label samples
 
+        We are given crisis data for different events - there are tweets for each
+        To do data augmentation:
+            - Choose crisis randomly
+            - Choose a random number of tweets in a range (2,6)
+            - ['this', 'tweet'] -> [0,0]
+            - Concatenate all tweets together to make a token-level dataset.
         """
 
         # Get crisis tokens and labels
@@ -143,7 +189,7 @@ class Loader(object):
         final_tokens_labels = [[],[]]
         for i,l in enumerate(crisis_labels):
             s = crisis_tokens[i]
-            new_label = [np.ones(len(sentence))*self.le.transform([label])[0]
+            new_label = [(np.ones(len(sentence))*self.le.transform([label])[0]).astype(int)
                          for sentence,label in zip(s, l)]
 
             #Chain together tokens and broadcasted labels
@@ -160,43 +206,24 @@ class Loader(object):
 
         return final_tokens_labels
 
-    def __synthesize_data(self,batch_size):
-        """ Synthesize multilabel text dataset
-        and labels
+    def next_epoch(self, num_batches = 100,
+                   batch_size = 64):
+        """Create a set of new data_aug batches for
+        experimentation and training
 
-        :returns: Synthesized train and test dataset
+        :num_batches: number of batches in an epoch
+        :returns: list of batches
 
         """
 
-        # Get varying sample sizes
-        sample_sizes = np.random.randint(self.co_occ_range[0],
-                                         self.co_occ_range[1],
-                                         size = batch_size,
-                                         dtype = int)
+        print("Creating {} batches of {} samples for next epoch"
+              .format(num_batches,batch_size))
+        epoch = []
+        for i in tqdm(range(num_batches)):
+            epoch.append(l.next_batch(batch_size = batch_size))
 
-        #Sample only from specific crises
-        sample_crises = np.random.choice(self.train_crises,size = batch_size)
+        return epoch
 
-        #Get specific random samples from every crisis
-        crisis_inds = [
-            np.random.randint(0, self.train_dict[c].shape[0],size = s)
-            for c,s in zip(sample_crises, sample_sizes)]
-
-        # Get a set of tuples joining sentences and labels together.
-        # These are unexpanded and are sent compressed to next_batch
-        # for expansion
-        crisis_tokens_labels = list(zip(*
-            [([self.tokenizer(sentence) for sentence
-               in self.train_dict[c].loc[s,"tweet_text"].tolist()],
-             self.train_dict[c].loc[s,"class_label"].tolist()) for c,s in
-            zip(sample_crises, crisis_inds)
-            ]))
-
-        # Separate crisis tokens and labels
-        crisis_tokens = crisis_tokens_labels[0]
-        crisis_labels = crisis_tokens_labels[1]
-
-        return crisis_tokens, crisis_labels
 
 #######################################################################
 # Testing
@@ -204,13 +231,32 @@ class Loader(object):
 
 if __name__ == "__main__":
     l = Loader()
-    # print(l.train_dirs )
     l.load_files()
+
     # print(l.train_corpus.columns)
     # print(l.test_corpus.shape)
     # print(l.train_corpus.shape)
     # print(l.dev_corpus.shape)
     # print(l.dev_crises)
     # print(l.train_dict.keys())
-    for i in tqdm(range(1000)):
-        l.next_batch(batch_size = 64)
+
+    ##Visually inspect a sample
+    #tokens_labels = l.next_batch(batch_size = 64)
+    #sample = tokens_labels[0][0]
+    #sample = [i.encode('cp1252') for i in sample]
+    #print(sample)
+    #print(tokens_labels[1][0])
+    #print(l.le.inverse_transform(tokens_labels[1][0]))
+    #sys.exit(1)
+
+    # Test of creating an epoch
+    l.next_epoch()
+
+# TODO: NOTES FOR EDGAR
+edgar_notes ='''
+To train with this for unilabel -> tokenize single tweets and provide label (broadcast)
+for multilabel ->
+- Per epoch, generate some number of batches (100)
+- Iterate and train through batches
+- Make new set of batches for the next epoch
+'''
